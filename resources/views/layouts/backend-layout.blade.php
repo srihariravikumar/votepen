44d0002a7123c5b3c5cb8e7f1bd6ec4508be66fd
Yoginth <yoginth@aol.com>
2017-11-21 17:51:02 +0530

    Added Font Awesome

--- resources/views/layouts/backend-layout.blade.php
+++ resources/views/layouts/backend-layout.blade.php
@@ -16,8 +16,8 @@
 
     @yield('head')
         <link href="/icons/css/fontello.6.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.0/css/bulma.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/bulma.css">
+        <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
         <link rel="stylesheet" href="{{ mix('/css/admin.css') }}">
     @include('user.user-style')
 
