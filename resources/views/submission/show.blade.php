217db89935edb0d808f1a0d58289283bb585f688
Yoginth <yoginth@aol.com>
2017-11-23 12:41:24 +0000

    Updated Tagvote --> Votepen

--- resources/views/submission/show.blade.php
+++ resources/views/submission/show.blade.php
@@ -2,25 +2,25 @@
 
 
 @section('head')
-	<title>{{ $submission->title }} - Tagvote</title>
+	<title>{{ $submission->title }} - Votepen</title>
 	<meta property="og:type" content="article" />
-	<meta property="og:title" content="{{ $submission->title }} - Tagvote" />
+	<meta property="og:title" content="{{ $submission->title }} - Votepen" />
 	<meta property="og:url" content="{{ config('app.url') }}/c/{{ $submission->category_name }}/{{ $submission->slug }}" />
-	<meta property="og:site_name" content="Tagvote" />
+	<meta property="og:site_name" content="Votepen" />
 
 	<meta name="twitter:card" content="summary" />
-	<meta name="twitter:site" content="@tagvote" />
-	<meta name="twitter:title" content="{{ $submission->title }} - Tagvote" />
+	<meta name="twitter:site" content="@votepen" />
+	<meta name="twitter:title" content="{{ $submission->title }} - Votepen" />
 
 	@if ($submission->type == "text")
 		<meta name="description" content="{{ $submission->data['text'] }}"/>
 		<meta property="og:description" content="{{ $submission->data['text'] }}" />
 		<meta name="twitter:description" content="{{ $submission->data['text'] }}" />
-		<meta property="og:image" content="/imgs/tagvote.svg">
-		<meta name="twitter:image" content="/imgs/tagvote.svg" />
+		<meta property="og:image" content="/imgs/votepen.svg">
+		<meta name="twitter:image" content="/imgs/votepen.svg" />
 	@else
-		<meta property="og:image" content="{{ $submission->data['thumbnail'] ?? $submission->data['path'] ?? '/imgs/tagvote.svg' }}" />
-		<meta name="twitter:image" content="{{ $submission->data['thumbnail'] ?? $submission->data['path'] ?? '/imgs/tagvote.svg' }}" />
+		<meta property="og:image" content="{{ $submission->data['thumbnail'] ?? $submission->data['path'] ?? '/imgs/votepen.svg' }}" />
+		<meta name="twitter:image" content="{{ $submission->data['thumbnail'] ?? $submission->data['path'] ?? '/imgs/votepen.svg' }}" />
 	@endif
 @stop
 
