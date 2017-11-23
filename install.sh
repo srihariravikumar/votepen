5b0470456ca9c5e3af49aa74e6b5f16aeb386057
Yoginth <yoginth@aol.com>
2017-11-23 14:27:05 +0000

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+. ~/.nvm/nvm.sh
+
+sudo apt-get update
+sudo apt-get install apache2
+sudo systemctl restart apache2
+sudo apt-get install curl
+sudo apt-get install mysql-server
+sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql
+sudo systemctl restart apache2
+sudo apt-get install php-cli
+sudo apt-get install phpmyadmin
+sudo apt-get install -y python-software-properties
+sudo add-apt-repository -y ppa:ondrej/php
+
+sudo apt-get update -y
+sudo apt-get install php7.1
+sudo apt-get install php7.1-mbstring
+sudo apt-get install php7.1-xml
+sudo apt-get install php7.1-curl
+sudo apt-get install composer
+
+nvm use 9.2.0
+npm run production
+php artisan serve
